443c3e768486163af1dcec3fe8d2223642e2e2e0
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-17 10:25:35 -0800

    preliminary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'products#index'
+
+  resources :products
+  resources :categories, except: [:destroy]
+  resources :orders
+  resources :orderitems
+  resources :users, except: [:destroy]
+
 end
