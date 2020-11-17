3ae5120b183ba5d608e5a58ef73faef929ebc83f
christinaminh <christina.minh@gmail.com>
2020-11-17 12:44:15 -0800

    Added nested orderitems route under product route to allow adding orderitem to cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products
+  resources :products do
+    resources :orderitems, only: [:index, :new]
+  end
   resources :categories, except: [:destroy]
   resources :orders
   resources :orderitems
