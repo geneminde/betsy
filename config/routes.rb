671eb0943d5a2872cb3988001ea5d0e04033c093
Anna Kim <anna.kim93@gmail.com>
2020-11-17 15:51:45 -0500

    adding omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  # Omniauth
+  get '/auth/github', as: 'github_login'
+  get '/auth/:provider/callback', to: 'users#create', as: 'omniauth_callback'
+
   resources :products
   resources :categories, except: [:destroy]
   resources :orders
