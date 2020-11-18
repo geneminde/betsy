f1132689dfc23ecbcb60e84e9d6ab1d7a7b7b3e9
Anna Kim <anna.kim93@gmail.com>
2020-11-18 00:42:13 -0500

    adjusting routes for user logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   get '/auth/github', as: 'github_login'
   get '/auth/:provider/callback', to: 'users#create', as: 'omniauth_callback'
 
-  post '/logout', to: 'users#logout', as: :logout
+  delete '/logout', to: 'users#destroy', as: :logout
   get '/users/current', to: 'users#current', as: :current_user
 
   resources :products
   resources :categories, except: [:destroy]
   resources :orders
   resources :orderitems
-  resources :users, except: [:destroy]
+  resources :users
 
 end
