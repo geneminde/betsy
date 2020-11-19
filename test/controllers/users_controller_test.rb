f6390ff6a30d96982699c4fae14d792ca7fd19d6
Anna Kim <anna.kim93@gmail.com>
2020-11-18 19:31:52 -0500

    users controller tests for show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe 'show' do
+    it "responds with success when showing an existing valid user" do
+      get user_path(User.first.id)
+      must_respond_with :success
+    end
+
+    it "will redirect when passed an invalid user id" do
+      get user_path(-1)
+      must_respond_with :redirect
+    end
+  end
+
 end
