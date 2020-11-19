d7fd231d9192565cd0bcc38c2ae5d0289496732b
christinaminh <christina.minh@gmail.com>
2020-11-19 15:38:17 -0800

    Added helper method to OrderItemsController to update item from product page if product's item is already in the cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,14 +1,12 @@
 class OrderItemsController < ApplicationController
   skip_before_action :require_login
   before_action :find_order_item, only: [:update, :destroy]
-
   before_action :has_cart?, only: [:create, :update]
+  before_action :consolidate_cart, only: [:create]
 
   # Add item to order/cart.html.erb
   def create
     @product = Product.find_by(id: params[:product_id])
-
-    consolidate_cart(@product)
 
     order_quantity = params[:quantity].to_i
 
@@ -31,26 +29,7 @@ class OrderItemsController < ApplicationController
 
 # Update quantity of item in order/cart
   def update
-    order_quantity = params[:order_item][:quantity].to_i
-    puts "#{order_quantity} order_items before update"
-
-    if @cart && @order_item
-      if @order_item.update(quantity: order_quantity)
-        flash[:success] = "Successfully updated order cart"
-        puts "changed to #{@order_item.quantity}"
-        redirect_to order_path(@cart.id)
-        return
-      else
-        flash[:error] = "A problem occurred. Could not update item in cart"
-        flash[:validation_error] = @order_item.errors.messages
-        redirect_back(fallback_location: root_path)
-        return
-      end
-    else
-      flash[:error] = "A problem occurred. Could not update item in cart"
-      redirect_back(fallback_location: root_path)
-      return
-    end
+    save_update(@order_item)
   end
 
   # Remove item from order/cart
@@ -93,9 +72,35 @@ class OrderItemsController < ApplicationController
     end
   end
 
-  def consolidate_cart(product)
-    if @cart && @cart.products.include?(product)
-      redirect_to action: :update
+  def consolidate_cart
+    @product = Product.find_by(id: params[:product_id])
+
+    if @cart && @cart.products.include?(@product)
+      existing_order_item = OrderItem.where(product: @product, order: @cart)
+
+      # Modify the params hash to work with create and update actions
+      params[:order_item] = { quantity: params[:quantity] }
+
+      save_update(existing_order_item)
+    end
+  end
+
+  def save_update(order_item)
+    if @cart && order_item
+      if order_item.update(quantity: params[:order_item][:quantity].to_i)
+        flash[:success] = "Successfully updated order cart"
+        redirect_to order_path(@cart.id)
+        return
+      else
+        flash[:error] = "A problem occurred. Could not update item in cart"
+        flash[:validation_error] = order_item.errors.messages
+        redirect_back(fallback_location: root_path)
+        return
+      end
+    else
+      flash[:error] = "A problem occurred. Could not update item in cart"
+      redirect_back(fallback_location: root_path)
+      return
     end
   end
 end
