9552f1624849e0b4407ae77bb99c79f5eb77377e
Anna Kim <anna.kim93@gmail.com>
2020-11-18 22:50:17 -0500

    add test for logging in existing user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 describe UsersController do
 
+  let (:user) { User.first }
+
   describe 'show' do
     it "responds with success when showing an existing valid user" do
       get user_path(User.first.id)
@@ -25,6 +27,26 @@ describe UsersController do
       get current_user_path
       must_respond_with :redirect
       expect(flash[:error]).must_equal 'Please log in to perform this action.'
+    end
+  end
+
+  describe 'login' do
+    it "logs in an existing user and redirects to the root route" do
+      start_count = User.count
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      get omniauth_callback_path(:github)
+      must_redirect_to root_path
+
+      expect(session[:user_id]).must_equal user.id
+
+      expect(User.count).must_equal start_count
+    end
+
+    it "creates an account for a new user and redirects to the root route" do
+    end
+
+    it "redirects to the login route if given invalid user data" do
     end
   end
 end
