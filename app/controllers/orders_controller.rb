a49a4b2173da4e4d7553eed93fd7d9971f8062f3
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-18 16:40:48 -0800

    controller show method + views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+
+  def show
+    order_id = session[:order_id]
+    @order = Order.find_by(id: order_id)
+
+    if @order.nil? || Order.empty_cart?(@order)
+      redirect_to root_path
+    end
+  end
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_items
 
+  def subtotal
+    self.order_items.inject(0) { |memo, item| memo + item.product.price }
+  end
 end
