1b56cd139c73ea7e86bee0ba96a762efd67985c1
Anna Kim <anna.kim93@gmail.com>
2020-11-18 19:50:48 -0500

    user model instantiation and relationship tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:user) { User.first }
+
+  it 'can be instantiated with the required fields' do
+    expect(user.valid?).must_equal true
+
+    %w[username email uid provider].each do |field|
+      expect(user).must_respond_to field
+    end
+  end
+
+  describe 'relationships' do
+    it 'has many products' do
+      expect(user.products.count).must_equal 1
+
+      user.products.each do |product|
+        expect(product).must_be_instance_of Product
+      end
+    end
+
+    it 'has many order items through products' do
+      expect(user.order_items.count).must_equal 1
+
+      user.order_items.each do |order_item|
+        expect(order_item).must_be_instance_of OrderItem
+      end
+    end
+
+    it 'has many categories through products' do
+
+    end
+  end
+
+  describe 'validations' do
+
+  end
+
+  describe 'custom methods' do
+
+  end
 end
