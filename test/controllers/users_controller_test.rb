d0097f17d99b64054cf56f055e54a995b4d0b710
Anna Kim <anna.kim93@gmail.com>
2020-11-18 19:35:28 -0500

    users controller test for current; need to revise one of current tests for login helper

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,4 +14,17 @@ describe UsersController do
     end
   end
 
+  describe 'current user' do
+    # it 'can return user page if a user is logged in' do
+    #   login
+    #   get current_user_path
+    #   must_respond_with :success
+    # end
+
+    it 'redirects if user is not logged in' do
+      get current_user_path
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal 'Please log in to perform this action.'
+    end
+  end
 end
