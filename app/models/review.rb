b176bd592ae093ea7dffe82a310d81b94c9a4043
Anna Kim <anna.kim93@gmail.com>
2020-11-23 16:14:10 -0500

    adjust validation to force rating to be a number from 1-5 not 0-5

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,9 +4,9 @@ class Review < ApplicationRecord
 
   validates :rating,
             presence: true,
-            numericality: { greater_than_or_equal_to: 0 },
+            numericality: { greater_than: 0 },
             inclusion: {
-                in: 0..5,
-                message: 'Please provide a rating from 0-5.'
+                in: 1..5,
+                message: 'Please provide a rating from 1-5.'
             }
 end
