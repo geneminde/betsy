f08ff2f9dfb17c39e336857c748296d33438f795
Anna Kim <anna.kim93@gmail.com>
2020-11-23 15:58:29 -0500

    adding product model validation tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -8,7 +8,81 @@ describe Product do
   end
 
   describe 'validations' do
+    it 'must have a name' do
+      product.name = nil
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :name
+    end
 
+    it 'must have a unique name on create' do
+      product = Product.new(name: Product.last.name,
+                            description: 'some description',
+                            price: 10,
+                            quantity: 10
+                            )
+
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :name
+      expect(product.errors.messages[:name]).must_equal ['has already been taken']
+    end
+
+    it 'does not need a unique name on update' do
+      product.name = Product.last.name
+      expect(product.valid?).must_equal true
+    end
+
+    it 'must have a description' do
+      product.description = nil
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :description
+    end
+
+    it 'must have a unique description on create' do
+      product = Product.new(name: 'test name',
+                            description: Product.last.description,
+                            price: 10,
+                            quantity: 10
+      )
+
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :description
+      expect(product.errors.messages[:description]).must_equal ['has already been taken']
+    end
+
+    it 'does not need a unique description on update' do
+      product.name = Product.last.description
+      expect(product.valid?).must_equal true
+    end
+
+    it 'must have a price' do
+      product.price = nil
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :price
+    end
+
+    it 'must have a price greater than or equal to 0' do
+      product.price = -90
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :price
+
+      product.price = 0
+      expect(product.valid?).must_equal true
+    end
+
+    it 'must have a quantity' do
+      product.quantity = nil
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :quantity
+    end
+
+    it 'must have a quantity greater than or equal to 0' do
+      product.quantity = -80
+      expect(product.valid?).must_equal false
+      expect(product.errors.messages).must_include :quantity
+
+      product.quantity = 0
+      expect(product.valid?).must_equal true
+    end
   end
 
   describe 'custom methods' do
