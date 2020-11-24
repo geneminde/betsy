28881687dd725dc836be46afa8435f76f6f0676d
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-24 12:44:20 -0800

    change date/time display on confirmation page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     return "[unknown]" unless date
     return content_tag(:span,"#{date.strftime("%b %m, %Y")}", class: 'date', title: date.to_s)
   end
+
+  def readable_time(date)
+    return "[unknown]" unless date
+    return content_tag(:span, "#{date.strftime("%I:%M:%S %p")}")
+  end
 end
