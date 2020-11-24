ba5a845607c90392c1bd8949e283ed4d354e8051
Anna Kim <anna.kim93@gmail.com>
2020-11-24 11:46:11 -0500

    add helper verify_authorized for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,17 @@
 class ProductsController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
   before_action :find_product, only: [:show, :edit, :retire, :update]
+  before_action :verify_authorized, except: [:index, :new, :create]
 
   def index
     @products = Product.all
   end
 
-  def show; end
+  def show
+    if @product.is_retired
+      verify_authorized
+    end
+  end
 
   def edit; end
 
@@ -53,6 +58,14 @@ class ProductsController < ApplicationController
         :quantity,
         :available,
         :user_id
-    )
+      )
+  end
+
+  def verify_authorized
+    unless session[:user_id] && User.find_by(id: session[:user_id]).check_own_product(@product)
+      flash[:error] = 'Uh oh! That product could not be found... Please try again.'
+      redirect_back(fallback_location: products_path)
+      return
+    end
   end
 end
