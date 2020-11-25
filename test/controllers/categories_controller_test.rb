78e3e947ac6e9323f7fa5c65c253e8db4b807099
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-24 20:04:03 -0800

    remove unused category controller test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,12 +1,6 @@
 require "test_helper"
 
 describe CategoriesController do
-  describe 'index' do
-    it 'gets the index page' do
-      get categories_path
-      must_respond_with :success
-    end
-  end
 
   describe 'show' do
     it 'gets the show page' do
